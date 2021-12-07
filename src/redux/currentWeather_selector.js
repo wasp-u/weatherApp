1ac33565f7271cd59b08328c76c211fafe9f70ac
“wasp” <osadchiy.u@gmail.com>
2021-12-08 00:48:29 +0200

    selector bug fix

--- src/redux/currentWeather_selector.js
+++ src/redux/currentWeather_selector.js
@@ -3,15 +3,15 @@ export const getForecast4Hours = (state) => {
     let result = []
 
     if (state.searchData.forecastWeatherData) {
-        let currentTime = state.searchData.forecastWeatherData.location.localtime.split(' ')[1].split(':')[0]
+        let currentTime = +state.searchData.forecastWeatherData.location.localtime.split(' ')[1].split(':')[0]
         result = state.searchData.forecastWeatherData.forecast.forecastday[0].hour.filter(item => {
-            if (item.time.split(' ')[1].split(':')[0] >= currentTime && item.time.split(' ')[1].split(':')[0] < +currentTime + 5) return true
+            if (+item.time.split(' ')[1].split(':')[0] > currentTime && +item.time.split(' ')[1].split(':')[0] <= currentTime + 5) return true
             else return false
         })
         if (result.length < 5) {
-            for (let i = 0; i <= 5 - result.length; i++) {
-                result.push(state.searchData.forecastWeatherData.forecast.forecastday[1].hour[i])
-            }
+            result = [...result, ...(state.searchData.forecastWeatherData.forecast.forecastday[1].hour.filter((item, index, arr) => {
+                return (index <= 4 - result.length) ? true : false
+            }))]
         }
     }
 
